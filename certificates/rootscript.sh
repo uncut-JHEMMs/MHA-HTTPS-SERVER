8d32f6dd605540f568d0139216a042320e27bdfa
huzaifa4884 <huzaifa.af@gmail.com>
2022-02-10 18:55:47 -0500

    All certificates added

---
+++ certificates/rootscript.sh
@@ -0,0 +1,16 @@
+#bin/bash!
+
+mkdir -p ca/root-ca/private ca/root-ca/db crl certs
+chmod 700 ca/root-ca/private
+
+cp /dev/null ca/root-ca/db/root-ca.db
+cp /dev/null ca/root-ca/db/root-ca.db.attr
+
+echo 01 > ca/root-ca/db/root-ca.crt.srl
+echo 01 > ca/root-ca/db/root-ca.crl.srl
+
+
+openssl req -new -config rootconf.cnf -out ca/root-ca.csr -keyout ca/root-ca/private/root-ca.key
+
+
+
